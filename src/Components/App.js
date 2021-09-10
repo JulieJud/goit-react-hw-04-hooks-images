3c868b4463481114cefaa3b1c7524c59f71d11f1
Julie Judachina <iuliia.saunova@email.cz>
2021-09-11 01:52:07 +0200

    https zapros

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Component } from 'react';
 import './App.css';
 import SearchBar from './Searchbar/Searchbar';
@@ -7,10 +8,21 @@ export class App extends Component {
     imageName: null,
     images: [],
     reqStatus: 'idle',
+    pageNumber: 1,
   };
+
   handleFormSubmit = imageName => {
     this.setState({ imageName });
   };
+
+  async componentDidUpdate(_, prevState) {
+    if (prevState.imageName !== this.state.imageName) {
+      const response = await axios.get(
+        `https://pixabay.com/api/?q=${this.state.imageName}&page=${this.state.pageNumber}&key=22647650-093efc913fe4b5bfd764725e8&image_type=photo&orientation=horizontal&per_page=12`,
+      );
+      console.log(response);
+    }
+  }
 
   render() {
     return (
