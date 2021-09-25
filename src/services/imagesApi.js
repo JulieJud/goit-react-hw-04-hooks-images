baf8262c479a6e86579b005bb34e3f1a5a89b468
Julie Judachina <iuliia.saunova@email.cz>
2021-09-25 02:59:11 +0200

    update axious

--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+//axios.defaults.baseURL = 'https://pixabay.com/api';
+
+const getImageByAxious = axios.create({
+  baseURL: 'https://pixabay.com/api',
+  params: {
+    key: '22647650-093efc913fe4b5bfd764725e8',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: '12',
+  },
+});
 
 const fetchImages = async (imageName, pageNumber) => {
-  const { data } = await axios.get(
-    `/?q=${imageName}&page=${pageNumber}&key=22647650-093efc913fe4b5bfd764725e8&image_type=photo&orientation=horizontal&per_page=12`,
+  const { data } = await getImageByAxious(
+    `/?q=${imageName}&page=${pageNumber}`,
   );
   console.log(data);
   return data.hits;
